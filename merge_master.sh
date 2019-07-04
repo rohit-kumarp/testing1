13f16ea5e5be48aa486d85e1a301e506799148a8
rohitK <rohit.kumar@freshworks.com>
2019-07-05 03:18:05 +0530

    merged with master

--- merge_master.sh
+++ merge_master.sh
@@ -12,7 +12,6 @@ if ! git checkout -b $branch; then
   echo "*** error: Failed to create temp branch ***"
   exit 1
 fi
-else git checkout $branch
 fi
 
 if ! git fetch origin master && git merge FETCH_HEAD --no-ff  --no-edit; then
