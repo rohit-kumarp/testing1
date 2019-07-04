1e49323cb868859114f8b977e6594bf0bd89d283
rohitK <rohit.kumar@freshworks.com>
2019-07-04 18:02:24 +0530

    merged with master

--- merge_master.sh
+++ merge_master.sh
@@ -1,2 +1,27 @@
 branch=$1
-echo "*** Current Working branch is : \"$branch\" ***"
+echo "*** Pull Request branch is : \"$branch\" ***"
+echo "*** creating temp branch with Pull Request & Merge with Latest Master"
+
+if [ -z "$branch" ]; then
+  echo "*** error: Failed to get PR branch *** "
+  exit 1
+fi
+
+if ! git checkout -b $branch; then
+  echo "*** error: Failed to create temp branch ***"
+  exit 1
+fi
+
+if ! git fetch origin master && git merge FETCH_HEAD --no-ff  --no-edit; then
+  echo "*** error: Failed to merge with latest branch ***"
+  exit 1
+fi
+
+echo "*** pushing ${branch} to git repo with master ***"
+
+if ! git push origin ${Branch}; then
+  echo "*** error: Failed to push temp branch \"${branch}\" in git repo ***"
+  exit 1
+fi
+
+echo "*** Successfully Pushed PR temp branch \"${branch}\""
