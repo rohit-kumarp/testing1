4b29d48d143d53cfe98dbf5e0b0441524805e909
rohitK <rohit.kumar@freshworks.com>
2019-07-06 01:02:05 +0530

    merged with master

--- merge_master.sh
+++ merge_master.sh
@@ -7,12 +7,14 @@ if [ -z "$branch" ]; then
   exit 1
 fi
 
-if ! git checkout $branch && git pull; then
-if ! git checkout -b $branch && git pull; then
+if ! git checkout $branch; then
+if ! git checkout -b $branch; then
   echo "*** error: Failed to create temp branch ***"
   exit 1
 fi
 fi
+
+git Pull
 
 if ! git fetch origin master && git merge FETCH_HEAD --no-ff  --no-edit; then
   echo "*** error: Failed to merge with latest branch ***"
@@ -21,7 +23,7 @@ fi
 
 echo "*** pushing ${branch} to git repo with master ***"
 
-if ! git push https://github.com/rohitAutomation/testing1.git ${Branch} --no-ff  --no-edit; then
+if ! git push https://github.com/rohitAutomation/testing1.git ${Branch} ; then
   echo "*** error: Failed to push temp branch \"${branch}\" in git repo ***"
   exit 1
 fi
