f90bb048c8667118882ac08667d4f7cbcd311d70
rohitK <rohit.kumar@freshworks.com>
2019-07-06 01:06:23 +0530

    merged with master

--- merge_master.sh
+++ merge_master.sh
@@ -23,7 +23,7 @@ fi
 
 echo "*** pushing ${branch} to git repo with master ***"
 
-if ! git push https://github.com/rohitAutomation/testing1.git ${Branch} ; then
+if ! git push https://github.com/rohitAutomation/testing1.git ${Branch} -f; then
   echo "*** error: Failed to push temp branch \"${branch}\" in git repo ***"
   exit 1
 fi
